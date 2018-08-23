c668cb1eddeb2b5f8f5b26ee401defc5aae997b4
Ezequiel Pássaro <epassaro15@gmail.com>
2018-08-23 14:04:25 -0300

    Using getopts for options.

--- reduction/src/puma_help.sh
+++ reduction/src/puma_help.sh
@@ -3,11 +3,11 @@
 echo " Description goes here." 
 echo; echo
 echo -e " \e[1;7m USAGE \e[0m"; echo
-echo -e " \e[1m puma\e[0m  <pulsar_name>  [OPTIONS]"
+echo -e " \e[1m puma\e[0m  \e[4m[OPTIONS]\e[0m  <pulsar_name>"
 echo; echo 
 echo -e " \e[1;7m OPTIONS \e[0m"; echo
-echo -e " \e[1m-p, --pfd-only\e[0m              Skip PRESTO script."
-echo; echo
+echo -e " \e[1m -p\e[0m                Skip PRESTO script."
+echo
 
 return
 
